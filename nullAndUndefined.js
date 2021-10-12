e18ce00e471d058ebdd04e0fdb81b869c1d7e457
bennu <benarzee1212@gmail.com>
2021-10-12 19:05:42 +0530

    new

---
+++ nullAndUndefined.js
@@ -0,0 +1,13 @@
+//In JavaScript, undefined is a type, whereas null an object.
+// undefined---It means a variable declared, but no value has been assigned a value.
+//null---Whereas, null in JavaScript is an assignment value. You can assign it to a variable.
+
+//undefined
+var str
+console.log(str)
+
+//null
+test=null
+console.log(test)
+
+
